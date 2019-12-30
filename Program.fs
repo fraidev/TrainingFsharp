b4bd0902da124556d46774d9895b440c36708112
Felipe Cardozo <fcrcardozo@outlook.com>
2019-12-29 23:04:57 -0300

    init

---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System
+
+module BasicFunctions = 
+     let sampleFunction1 x = x*x + 3
+     
+[<EntryPoint>]
+let main argv = 
+    printfn "Hello World from F#!"
+    let num = BasicFunctions.sampleFunction1(2)
+    printfn "%d" num
+    
+    
+    
+    
+    0 // return an integer exit code
